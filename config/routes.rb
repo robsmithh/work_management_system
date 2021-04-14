2a04fa0f0b8ddfd38d87a03a0b23e0e87752f691
Rob Smith <robsmithh@live.co.uk>
2021-04-14 08:59:01 +0100

    add devise gem, and set up including views

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+
+
+  # devise_for :users
+  # devise_for :users, :controllers => {:users => "users_controller"}
+  devise_for :user, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root 'pages#home'
+  root to: 'pages#home'
 
   resources :contacts
-
-  resources :users
 
 end
