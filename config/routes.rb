5f00bf1632c108a69f2cbfb4321ecf402d0c537b
Rob Smith <robsmithh@live.co.uk>
2021-04-15 19:45:30 +0100

    add work_request routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
 
-
-  # devise_for :users
-  # devise_for :users, :controllers => {:users => "users_controller"}
   devise_for :user, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" }
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: 'pages#home'
 
   resources :contacts
 
+  resources :work_requests
 end
